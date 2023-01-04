68a7ad6c2dcd9d6300be3c8b35becc470500769a
anwarul <anwarulhoque032@gmail.com>
2023-01-04 17:46:23 +0600

    doctor portal bannaer add

---
+++ src/Pages/Home/InfoCards/InfoCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const InfoCard = ({card}) => {
+    const { icon, name, description, bgClass } = card;
+    return (
+        <div className={`card card-side p-6 text-white shadow-xl ${bgClass}`}>
+            <figure>
+                <img src={icon} alt="Movie" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{description}</p>
+                
+            </div>
+        </div>
+    );
+};
+
+export default InfoCard;
