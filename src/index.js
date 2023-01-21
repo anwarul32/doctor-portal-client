84d0fbd73cba0f272c05610b5c2720d96260775e
anwarul <anwarulhoque032@gmail.com>
2023-01-21 22:09:55 +0600

    added tanstack react query

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@ import './index.css';
 import App from './App';
 import 'react-day-picker/dist/style.css';
 import AuthProvider from './contexts/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+
+const queryClient = new QueryClient();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
+  // </React.StrictMode>
 );
 
 
